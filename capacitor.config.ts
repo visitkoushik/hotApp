8380bba0800c3a015e4d1158df9b1d7da1fcf5cf
Koushik Majumder <koushik@Koushiks-MacBook-Air.local>
2022-09-07 11:04:44 +0530

    Added Capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.chatabook.app',
   appName: 'chataBook',
   webDir: 'www',
   bundledWebRuntime: false
